68f193bf3e366a99c2864bc24c69d6137b29db0a
Erick <erickkan2004@gmail.com>
2024-07-13 22:50:55 -0300

    comment line

--- src/Journey.Infrastructure/Entities/Trip.cs
+++ src/Journey.Infrastructure/Entities/Trip.cs
@@ -1,5 +1,5 @@
 ﻿namespace Journey.Infrastructure.Entities;
-public class Trip
+public class Trip // pegar e setar trip do db browser
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
